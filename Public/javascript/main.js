4495c4d456a0ee91370893ab2b193000b9c05e23
IlliaH <danteoutoftime@gmail.com>
2021-08-30 21:18:39 -0400

    added action buttons to about me section

--- Public/javascript/main.js
+++ Public/javascript/main.js
@@ -1,3 +1,8 @@
 function sendEmail() {
+    window.open('mailto: illia.honcharenko95@gmail.com');
+}
+
+function openCV() {
     
+    window.open("file:///Users/dante/Documents/Books/Illia_Honcharenko_resume.docx");
 }
